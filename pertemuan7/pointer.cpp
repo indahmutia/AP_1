3b1325973f755e637b0224b0cf99293cfa353857
indahmutia <indahmutiatrigana@gmail.com>
2025-05-05 09:31:32 +0700

    pertemuan7

---
+++ pertemuan7/pointer.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+using namespace std;
+
+void penjumlahan(int a, int b) {
+    cout << a + b << endl;
+}
+
+void penjumlahanPointer (int *a, int *b) {
+    *a += *b;
+    cout << *a << endl;
+}
+
+void doublevalue ( int *a) {
+    *a *= 2;
+    cout << *a << endl;
+}
+
+
+int main() {
+    system("cls");
+
+    // Deklarasi Pointer
+    int number = 35;
+    int *pointer_number = &number;
+
+    // cout << "Isi variabel number = " << number << endl;
+    // cout << "Alamat memori variabel number = " << &number << endl;
+    // cout << "Isi variabel pointer number = " << pointer_number << endl;
+    // cout << "Isi variabel yang ditunjuk oleh pointer_number = " << *pointer_number << endl;
+    // cout << "Alamat memori variabel pointer_number = " << &pointer_number << endl;
+    // cout << endl;
+
+
+    // Pointer Operation
+    // *pointer_number = 25;
+    // cout << "Isi variabel number = " << number << endl;
+    // cout << "Alamat memori variabel number = " << &number << endl;
+    // cout << "Isi variabel pointer number = " << pointer_number << endl;
+    // cout << "Isi variabel yang ditunjuk oleh pointer_number = " << *pointer_number << endl;
+    // cout << "Alamat memori variabel pointer_number = " << &pointer_number << endl;
+    // cout << endl;
+
+
+    // Pointer in Array
+    int num[] = {1, 2, 3, 4, 5};
+    int *pointer_num = num;
+    // cout << "Isi variabel number = " << num[0] << endl;
+    // cout << "Alamat memori variabel num = " << &num[3] << endl;
+    // cout << "Isi variabel pointer_num = " << pointer_num << endl;
+    // cout << "Isi variabel yang ditunjuk oleh pointer_num = " << *pointer_num << endl;
+    // cout << "Alamat memori variabel pointer_num = " << &pointer_num << endl;
+    // cout << endl;
+
+    // *pointer_num += 5; // 6
+    // cout << "Isi variabel number = " << num[0] << endl;
+    // cout << "Alamat memori variabel num = " << &num[0] << endl;
+    // cout << "Isi variabel pointer_num = " << pointer_num << endl;
+    // cout << "Isi variabel yang ditunjuk oleh pointer_num = " << *pointer_num << endl;
+    // cout << "Alamat memori variabel pointer_num = " << &pointer_num << endl;
+    // cout << endl;
+
+
+    // Pointer in Parameter
+    // int num1 = 5;
+    // int num2 = 7;
+    // penjumlahan(num1, num2);
+    // cout << num1 << endl;
+    // cout << num2 << endl;
+    // penjumlahanPointer(&num1, &num2);
+    // cout << num1 << endl;
+    // cout << num2 << endl;
+
+    // Pointer in Pointer
+    // int score = 4;
+    // int *pointer_score = &score;
+    // int **ptr_pointer_score = &pointer_score;
+    // cout << "Isi variabel score " << score << " dan alamat memorinya " << &score << endl;
+    // cout << "Isi variabel pointer_score " << pointer_score << " dan isi variabel yang ditunjuk oleh pointer score " << *pointer_score << " alamat memori pointer_score " << &pointer_score << endl;
+    // cout << "Isi variabel ptr_pointer_score " << pointer_score << " dan isi variabel yang ditunjuk oleh ptr_pointer_score " << **ptr_pointer_score << " alamat memori ptr_score " << &ptr_pointer_score << endl;
+
+
+    // dynamic pointer 
+    int *ptr = new int;
+    *ptr = 30;
+    cout << "isi variabel ptr = " << *ptr << "alamat variabel ptr = "<< &ptr << endl;
+
+    delete ptr; // deallocation
+    cout << "isi variabel ptr = " << *ptr << "alamat variabel ptr = "<< &ptr << endl;
+
+    // contoh 
+    // int n;
+    // cout << "masukkan sebuah angka = ";
+    // cin >> n;
+
+    // doublevalue(&n);
+    // cout << "nilai angka setelah dikali 2 =" << n << endl;
+}
